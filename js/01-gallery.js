742ff0fe9f0faff7ddd5fed50018a0560e0d924e
StasMulyk <stasmulyk@gmail.com>
2022-07-09 23:58:54 +0300

    виправив 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,11 +25,27 @@ itemsOnList.addEventListener('click', modalShow);
 
 function modalShow(event) {
     event.preventDefault();
-    if (event.target.nodeName !== "img") { return; };
+    if (event.target.nodeName !== "IMG") { return; };
     {
         const modalWindow = basicLightbox.create(
             `<img src="${event.target.dataset.source}" width="800" height="600">`);
         modalWindow.show()
-    
     }
 };
+
+
+
+onShow: (instance) => {
+    window.addEventListener('click', click2)
+}
+
+onclose: (instance) => {
+    window.removeEventListener('click', click2)
+}
+
+function click2(event) {
+    console.log(event.target.classList.containts('basicLightbox'));
+    if (event.target.classList.contains('basicLightbox')) {
+        instance.close(window.removeEventListener('click', click2));
+    }
+}
